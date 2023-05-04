436c738796a222263e9cae21f1b7ae2733d895aa
yeyintmin17 <blone5128@gmial.com>
2023-05-04 16:46:57 +0630

    Top Sidebar, Content Area, Remove assets from repo > 3 May 2023

--- js/app.js
+++ js/app.js
@@ -6,3 +6,24 @@ $(document).ready(function(){
     });
     // End Left Side Bar
 });
+
+// Start JS Area
+
+// Start Top Sidebar
+
+// start notify & user logout
+
+// start dropdown
+
+function dropbtn(e){
+    // console.log(e.target.parentElement);
+    e.currentTarget.nextElementSibling.classList.toggle("show");
+}
+
+// end dropdown
+
+// end notify & user logout
+
+// End Top Sidebar
+
+// End JS Area
